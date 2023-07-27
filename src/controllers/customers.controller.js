3dd362e867b5fe61244650243ef5fce53411a246
Wilker Mizael <107046187+wilkermizael@users.noreply.github.com>
2023-07-27 16:23:49 -0300

    update customer

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,6 +1,6 @@
 import { db } from "../database/database.js";
 
-export default async function insertCustomers (req, res){
+export async function insertCustomers (req, res){
     const dataClients = req.body
     
     try{
@@ -10,10 +10,57 @@ export default async function insertCustomers (req, res){
             ,[dataClients.name, dataClients.phone, dataClients.cpf, dataClients.birthday])
             return res.sendStatus(201)
         }
-        //console.log(findCpf.rows)
         return res.sendStatus(409)
        
     }catch(error){
         res.status(500).send(error.message)
     }
 } 
+
+export async function listCustomers(req,res){
+    const {id} = req.params
+
+    try{
+        
+        if(!id){
+            const customers = await db.query(`SELECT * FROM customers;`)
+            return res.status(201).send(customers.rows)
+        
+        }
+        
+        const customers = await db.query(`SELECT * FROM customers WHERE id =$1;`,[id])
+        
+        if(customers.rows.length === 0){
+    
+            return res.sendStatus(404)
+        }else{
+            return res.status(201).send(customers.rows)
+        }
+        
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+}
+
+export async function updateCustomers(req,res){
+    const {id} = req.params
+    const dataUpdate = req.body
+    
+
+    try{
+        const customer = await db.query(`SELECT * FROM customers WHERE cpf = $1;`,[dataUpdate.cpf])
+       
+        if(customer.rows[0].id !== Number(id)){
+            return res.sendStatus(409)
+           
+        }
+        await db.query(`UPDATE customers SET name = $1,phone = $2,cpf= $3, birthday= $4 where id= $5;`,
+        [dataUpdate.name, dataUpdate.phone, dataUpdate.cpf, dataUpdate.birthday, id]
+
+        )
+        res.sendStatus(200)
+        
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import insertCustomers from "../controllers/customers.controller.js";
+import {insertCustomers, listCustomers, updateCustomers } from "../controllers/customers.controller.js";
+import { validadeSchema } from "../middlewares/validateSchema.js";
+import schemaCustomers from "../schema/customers.schema.js";
 
 const customersRouter = Router()
 
-customersRouter.post('/customers', insertCustomers)
+customersRouter.post('/customers',validadeSchema(schemaCustomers) ,insertCustomers)
+customersRouter.get('/customers',listCustomers)
+customersRouter.get('/customers/:id',listCustomers)
+customersRouter.put('/customers/:id',validadeSchema(schemaCustomers),updateCustomers)
 
 export default customersRouter
