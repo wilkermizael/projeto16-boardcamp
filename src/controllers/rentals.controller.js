bd6410f738af49a76c6f18e18df243572bb57a9c
Wilker Mizael <107046187+wilkermizael@users.noreply.github.com>
2023-07-30 20:36:02 -0300

    avaliador 1.8

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -91,14 +91,15 @@ export async function returnRentals(req, res){
         
         //CALCULANDO OS DIAS QUE ATRASO
         const date = returnRent.rows[0].rentDate
-        const diffDate = Number(dayjs().diff(date, 'day'))
+        const diffDate = Number(dayjs().format('YYYY-MM-DD').diff(date, 'day'))
         
         if(diffDate > returnRent.rows[0].daysRented){//CASO TENHA ATRASO
-            const delayFee = (diffDate - returnRent.rows[0].daysRented)*returnRent.rows[0].originalPrice //PREÇO A PAGAR PELO ATRASO
+            //const delayFee = (diffDate - returnRent.rows[0].daysRented)*returnRent.rows[0].originalPrice //PREÇO A PAGAR PELO ATRASO
+            const delayFee = diffDate*returnRent.rows[0].originalPrice
             await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"= $2 where id=$3;`,[dayjs(), delayFee, id])
             return res.sendStatus(200)
         }
-        await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"= $2 where id=$3;`,[dayjs(), 0, id])
+        await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"= $2 where id=$3;`,[dayjs().format('YYYY-MM-DD'), 0, id])
         res.sendStatus(200)
         //ATUALIZANDO A TABELA DEPOIS DA ENTREGA DO JOGO
         
